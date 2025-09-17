07a36e11284b74dafcec785ee3f51cb72fed9fdb
manishpachauri18 <panditmanish1437@gmail.com>
2025-09-17 13:07:14 +0530

    Updated App.js and added send-email API function

--- coming-soon/src/App.js
+++ coming-soon/src/App.js
@@ -52,7 +52,7 @@ function App() {
     };
 
     try {
-      const response = await fetch("https://cosmos-diva-backend.vercel.app/api/send-email", {
+      const response = await fetch("https://cosmos-diva.vercel.app/api/send-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(emailData),
